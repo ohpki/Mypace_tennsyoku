f2380d534708a3dd5533eecd41bb63bfd1f46145
ohpki <ohpkiki@gmail.com>
2022-03-31 12:01:23 +0000

    病院側の編集と退会機能

--- app/controllers/employer/hospitals_controller.rb
+++ app/controllers/employer/hospitals_controller.rb
@@ -1,18 +1,36 @@
 class Employer::HospitalsController < ApplicationController
+  before_action :authenticate_hospital!
+  before_action :set_current_hospital
+
+
   def show
-    @hospital = current_hospital
-    @job_informations = @hospital.job_informations.page(params[:page]).per(3)
+    @job_informations = @hospital.job_informations.order(created_at: :desc).page(params[:page]).per(3)
   end
 
   def edit
   end
 
   def update
+    @hospital.update(hospital_parameters)
+    if redirect_to employer_hospitals_path, notice: "プロフィールを更新しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @hospital.destroy
+    redirect_to root_path, notice: "退会しました"
   end
 
   def quit
+  end
+  private
+  def hospital_parameters
+    params.require(:hospital).permit(:name, :postal_code, :address, :nickname, :phone_number, :Introduction, :image, :email)
+  end
+
+  def set_current_hospital
+    @hospital = current_hospital
   end
 end
--- app/controllers/employer/registrations_controller.rb
+++ app/controllers/employer/registrations_controller.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 class Employer::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :address, :phone_number, :Introduction, :image])
-  end
 
   # GET /resource/sign_up
   # def new
@@ -42,7 +39,19 @@ class Employer::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :address, :phone_number, :Introduction, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal_code, :address, :phone_number, :Introduction, :image])
+  end
+
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
