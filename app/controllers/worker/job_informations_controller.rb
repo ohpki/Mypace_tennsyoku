4872bc4037cb183e1165dfcd2647c0d9dd0ba440
ohpki <ohpkiki@gmail.com>
2022-04-01 01:20:47 +0000

    求人情報一覧、応募機能

--- app/controllers/worker/job_informations_controller.rb
+++ app/controllers/worker/job_informations_controller.rb
@@ -1,6 +1,12 @@
 class Worker::JobInformationsController < ApplicationController
   def index
+    @job_informations = JobInformation.where(job_offer_status: true).order(created_at: :desc).page(params[:page]).per(10)
   end
   def show
+    @job_information = JobInformation.find(params[:id])
+    if @subscription = current_nurse.subscriptions.find_by(job_information_id: @job_information)
+    else
+      @subscription = Subscription.new
+    end
   end
 end
--- app/controllers/worker/subscriptions_controller.rb
+++ app/controllers/worker/subscriptions_controller.rb
@@ -1,11 +1,22 @@
 class Worker::SubscriptionsController < ApplicationController
+  before_action :authenticate_nurse!
   def new
+    @subscription = Subscription.new
+    @job_information = params[:subscription][:job_information_id]
   end
 
   def create
+    @subscription = Subscription.new(subscription_params)
+    if @subscription.save
+      redirect_to worker_job_informations_path, notice: "求人に応募しました"
+    else
+      redirect_back(fallback_location: root_path, notice: "応募処理に失敗しました")
+    end
   end
 
   def index
+    @subscriptions = current_nurse.subscriptions
+    @job_informations = @subscriptions.job_informations.order.order(created_at: :desc).page(params[:page]).per(3)
   end
 
   def show
@@ -18,5 +29,17 @@ class Worker::SubscriptionsController < ApplicationController
   end
 
   def destroy
+    @subscription = Subscription.find(params[:id])
+    if @subscription.destroy
+      redirect_to worker_job_informations_path, notice: "応募を取り消しました"
+    else
+      redirect_back(fallback_location: root_path, notice: "取り消しに失敗しました")
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:job_information_id, :nurse_id)
   end
 end
