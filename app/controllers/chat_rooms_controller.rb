5d778bc739f8b74bdceec0db8979cfcab1c79311
ohpki <ohpkiki@gmail.com>
2022-04-01 11:06:25 +0000

    看護師マイページ作成

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -6,8 +6,14 @@ class ChatRoomsController < ApplicationController
   end
 
   def create
+    @chat_room = ChatRoom.new(chat_room_params)
+    @chat_room.save
+    redirect_to new_chat_path
   end
 
-  def new
+
+  private
+  def chat_room_params
+    params.require(:chat_room).permit(:hospital_id, :nurse_id)
   end
 end
--- app/controllers/worker/hospitals_controller.rb
+++ app/controllers/worker/hospitals_controller.rb
@@ -1,5 +1,6 @@
 class Worker::HospitalsController < ApplicationController
   def show
     @hospital = Hospital.find(params[:id])
+    @chat_room = ChatRoom.new
   end
 end
