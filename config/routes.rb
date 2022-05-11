697a17f5c3c1aa381a301b92edb6516c5332c0dc
ohpki <ohpkiki@gmail.com>
2022-05-11 12:26:07 +0000

    Gemfile変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'images/index'
+  get 'images/new'
+  get 'images/edit'
+  get 'images/show'
   devise_for :admin, controllers: {
   sessions: 'admin/sessions'
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_081202) do
+ActiveRecord::Schema.define(version: 2022_05_07_043050) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -90,6 +90,13 @@ ActiveRecord::Schema.define(version: 2022_04_08_081202) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_hospitals_on_email", unique: true
     t.index ["reset_password_token"], name: "index_hospitals_on_reset_password_token", unique: true
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "image_id"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "job_informations", force: :cascade do |t|
